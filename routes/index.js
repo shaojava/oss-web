cb744b21a0925f5dcca79118af2afc6aa1d29d75
GeorgeXu <xuge123@gmail.com>
2015-05-06 23:20:02 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,22 @@ var router = express.Router();
 var httpProxy = require('http-proxy');
 var OSSSigner = require('../node_modules/aliyun-sdk/lib/signers/oss.js');
 
+var  tunnel = require('tunnel');
+
+var tunnelingAgent = tunnel.httpOverHttp({
+    proxy: {
+        host: 'localhost',
+        port: 8888
+    }
+});
+
 var credentials = {
     secretAccessKey:"GBJN7GarVWrITZT9YZR64Ir6bOLEM5",
     accessKeyId:"aNgmvBucXXcJnOgj"
 };
-var proxy = httpProxy.createProxyServer({});
+var proxy = httpProxy.createProxyServer({
+    agent:tunnelingAgent
+});
 
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
     var  signer = new OSSSigner(req);
@@ -34,8 +45,10 @@ router.get('/', function (req, res, next) {
 });
 
 router.all('/api', function (req, res, next) {
+    var target = 'http://' + (req.query['bucket'] ? req.query['bucket'] + "." : "") + (req.query['region'] ? req.query['region'] + '.' : '') + req.query['host'];
+    console.log('target',target);
     proxy.web(req, res, {
-        target: 'http://' + (req.query['bucket'] ? req.query['bucket'] + "." : "") + (req.query['region'] ? req.query['region'] + '.' : '') + req.query['host']
+        target: target
     });
 });
 
