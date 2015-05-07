67c4589063cafc1b92d473fedd78f0dc3e1fd248
Meteor <liuxingmatt@gmail.com>
2015-05-07 16:09:21 +0800

    基本修复完毕

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,6 @@ var OSSSigner = require('../node_modules/aliyun-sdk/lib/signers/oss.js');
 var request = require('request');
 require('request-debug')(request);
 
-//var tunnel = require('tunnel');
-
-//var tunnelingAgent = tunnel.httpOverHttp({
-//    proxy: {
-//        host: 'localhost',
-//        port: 8888
-//    }
-//});
-
 var credentials = {
     accessKeyId: "aNgmvBucXXcJnOgj",
     secretAccessKey: "GBJN7GarVWrITZT9YZR64Ir6bOLEM5"
@@ -37,52 +28,22 @@ router.get('/', function (req, res, next) {
 });
 
 router.all('/api', function (req, res, next) {
-    console.log('req.headers', req.headers);
-    //res.send();
     if (req.headers['x-proxy-host']) {
         req.headers['host'] = req.headers['x-proxy-host'];
-        //require('request').debug = true;
-        var requestUrl =   'http://' + req.headers['x-proxy-host'] + req.url.replace(/^\/api/,'');
-        if (req.method == 'GET') {
-            var requestOSS = request({
-                url: requestUrl,
-                method: req.method,
-                headers: req.headers
-            });
-
-            requestOSS.virtualHostedBucket =req.headers['x-proxy-bucket'] ? req.headers['x-proxy-bucket'] : '';
-            var signer = new OSSSigner(requestOSS);
-            signer.addAuthorization(credentials, new Date());
-            requestOSS.pipe(res);
-        } else {
-            console.log('req.body', req.rawBody);
-            var requestOSS = request({
-                url: requestUrl,
-                method: req.method,
-                body: req.rawBody,
-                headers: req.headers
-            });
-            requestOSS.virtualHostedBucket =req.headers['x-proxy-bucket'] ? req.headers['x-proxy-bucket'] : '';
-            var signer = new OSSSigner(requestOSS);
-            signer.addAuthorization(credentials, new Date());
-            console.log('requestOSS', requestOSS.headers['Authorization']);
-            requestOSS.pipe(res);
-        }
-        //var req2 = _.clone(req);
-        //req2.headers['host'] = ;
-        //if (req2.method == 'GET') {
-        //    var requestOSS2 = request({
-        //        url: target,
-        //        method: req2.method,
-        //        qs: req2.query,
-        //        headers: req2.headers
-        //    });
-        //    var signer = new OSSSigner(requestOSS2);
-        //    signer.addAuthorization(credentials, new Date());
-        //    console.log('requestOSS2', requestOSS2.headers['Authorization']);
-        //}
+        var requestUrl = 'http://' + req.headers['x-proxy-host'] + req.url.replace(/^\/api/, '');
+        var requestOSS = request({
+            url: requestUrl,
+            method: req.method,
+            headers: req.headers,
+            body: req.rawBody
+        });
+        requestOSS.virtualHostedBucket = req.headers['x-proxy-bucket'] ? req.headers['x-proxy-bucket'] : '';
+        var signer = new OSSSigner(requestOSS);
+        signer.addAuthorization(credentials, new Date());
+        requestOSS.pipe(res);
+    } else {
+        res.send(404);
     }
-
 });
 
 module.exports = router;
